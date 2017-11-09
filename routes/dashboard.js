be8e678664b31692bd714a1c869c4d44acdc2565
motosharpley <motosharpley@gmail.com>
2017-11-09 13:21:03 -0600

    added edit product func

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,120 +1,93 @@
 const express = require('express');
 const router = express.Router();
 const Product = require('../models/product');
-const Admin = require('../models/admin');
+const User = require('../models/user');
 
-// Dashboard route /db
-router.get('/', function(req, res, next) {
-  res.render('dashboard/index', { title: 'NodeCart' });
+
+router.use('/', ensureAuthenticated, function(req, res, next) {
+  next();
 });
 
-// Edit Article
-// router.get('/edit/:id', ensureAuthenticated, function(req, res){
-//     Article.findById(req.params.id, function(err, article){
-//         if(article.author != req.user._id){
-//             req.flash('danger', 'Not Authorized');
-//             res.redirect('/');
-//         }
-//         res.render('edit_article', {
-//             title:'Edit Article',
-//             article:article
-//         });
-//     });
-// });
-
-// update post route
-// router.post('/edit/:id', function(req, res){
-//     let article = {};
-//     article.title = req.body.title;
-//     article.body = req.body.body;
-//     article.author = req.body.author;
-
-//     let query = {_id:req.params.id}
-
-//     Article.update(query, article, function(err){
-//         if(err){
-//             console.log(err);
-//             return;
-//         } else {
-//             req.flash('success', 'Article Updated');
-//             res.redirect('/');
-//         }
-//     });
-// });
+// Dashboard route - /db
+router.get('/', function(req, res, next) {
+  Product.find(function(err, docs) {
+  res.render('dashboard/index', { title: 'Dashboard', products: docs });
+  });
+});
 
 // Add Product form Route
-router.get('/add_product', ensureAuthenticated, function(req, res){
-    res.render('dashboard/add_product', {
-        title:'Add Product'
-    });
+router.get('/add_product', function(req, res){
+  res.render('dashboard/add_product', {
+      title:'Add Product'
+  });
 });
 
 // Create Product Route
 router.post('/add_product', function(req, res){
-    req.checkBody('title', 'Title is required').notEmpty();
-    req.checkBody('description', 'Description is required').notEmpty();
-    req.checkBody('price', 'Price is required').notEmpty();
+  req.checkBody('title', 'Title is required').notEmpty();
+  req.checkBody('description', 'Description is required').notEmpty();
+  req.checkBody('price', 'Price is required').notEmpty();
 
-    // Get Errors
-    let errors = req.validationErrors();
+  // Get Errors
+  let errors = req.validationErrors();
 
-    if(errors){
-        res.render('add_product', {
-            title:'Add Product',
-            errors:errors
-        });
-    } else {
-        let product = new Product();
-        product.imagePath = req.body.imagePath;
-        product.title = req.body.title;
-        product.description = req.body.description;
-        product.price = req.body.price;
-    
-        product.save(function(err){
-            if(err){
-                console.log(err);
-                return;
-            } else {
-                req.flash('success', 'Product Added');
-                res.redirect('/');
-            }
-        });
-    } 
+  if(errors){
+      res.render('add_product', {
+          title:'Add Product',
+          errors:errors
+      });
+  } else {
+      let product = new Product();
+      product.imagePath = req.body.imagePath;
+      product.title = req.body.title;
+      product.description = req.body.description;
+      product.price = req.body.price;
+  
+      product.save(function(err){
+          if(err){
+              console.log(err);
+              return;
+          } else {
+              req.flash('success', 'Product Added');
+              res.redirect('/db');
+          }
+      });
+  } 
 });
 
-// Delete Article
-// router.delete('/:id', function(req, res){
-//     if(!req.user._id){
-//         res.status(500).send();
-//     }
+// Edit Product
+router.get('/edit/:id', function(req, res){
+    Product.findById(req.params.id, function(err, product){
+        res.render('dashboard/edit_product', {
+            title:'Edit Product',
+            product:product
+        });
+    });
+});
 
-//     let query = {_id:req.params.id}
+// update product route
+router.post('/edit/:id', function(req, res){
+    let product = {};
+    product.imagePath = req.body.imagePath;
+    product.title = req.body.title;
+    product.description = req.body.description;
+    product.price = req.body.price;
 
-//     Product.findById(req.params.id, function(err, product){
-//         if(product.Admin != req.user._id){
-//             res.status(500).send();
-//         } else {
-//             Article.remove(query, function(err){
-//                 if(err){
-//                     console.log(err);
-//                 }
-//                 res.send('Success');
-//             });  
-//         }
-//     });
-// });
+    let query = {_id:req.params.id}
 
-// Get Single Article
-// router.get('/:id', function(req, res){
-//     Article.findById(req.params.id, function(err, article){
-//         User.findById(article.author, function(err, user){
-//             res.render('article', {
-//                 article:article,
-//                 author: user.name
-//             });
-//         });        
-//     });
-// });
+    Product.update(query, product, function(err){
+        if(err){
+            console.log(err);
+            return;
+        } else {
+            req.flash('success', 'Product Updated');
+            res.redirect('/db');
+        }
+    });
+});
+
+// Delete Product
+
 
 // Access Control
 function ensureAuthenticated(req, res, next){
